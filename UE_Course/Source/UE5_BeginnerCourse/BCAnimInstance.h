8158cb3499c9e913ca54f8644e3f6bdeed8189c9
Flexflext <75678247+MaxKaul@users.noreply.github.com>
2023-11-08 12:01:07 +0100

    1. Class

---
+++ UE_Course/Source/UE5_BeginnerCourse/BCAnimInstance.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BCAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_BEGINNERCOURSE_API UBCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+	virtual void NativeBeginPlay() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY()
+	class AUE5_BeginnerCourseCharacter* player;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=PlayerComps, meta=(AllowPrivateAccess))
+		APlayerController* playerController;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = PlayerComps, meta = (AllowPrivateAccess))
+		float playerVelocity;
+};
---
+++ UE_Course/Source/UE5_BeginnerCourse/BCPlayerStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "BCPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_BEGINNERCOURSE_API ABCPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+};
